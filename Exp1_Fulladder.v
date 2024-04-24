87191c77e6d0938143485303c97ef2297a245608
YEMANTHKUMAR <160569469+YEMANTHKUMAR@users.noreply.github.com>
2024-04-24 09:21:17 +0530

    Update Exp1_Fulladder.v

--- Exp1_Fulladder.v
+++ Exp1_Fulladder.v
@@ -1,8 +1,11 @@
-module fa_ha(a,b,c,sum,carry);
+module fulladder(a,b,c,sum,carry);
 input a,b,c;
 output sum,carry;
-
-
-
-
+wire w1,w2,w3;
+xor x1(w1,a,b);
+xor x2(sum,w1,c);
+and x3(w2,a,b);
+and x4(w3,w1,w2);
+or x5(carry,w3,w2);
 endmodule
+
