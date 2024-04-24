afcad6768bf554e5788e09775b751236415453f4
YEMANTHKUMAR <160569469+YEMANTHKUMAR@users.noreply.github.com>
2024-04-24 09:22:18 +0530

    Update Exp1_Fullsubractor.v

--- Exp1_Fullsubractor.v
+++ Exp1_Fullsubractor.v
@@ -1,7 +1,10 @@
-module fs(a,b,bin,d,bout);
+module fullsubractor(a,b,bin,d,bout);
 input a,b,bin;
 output d,bout;
-
-
-
+wire w1,w2,w3;
+xor (d,a,w1);
+xor(w1,b,bin);
+and(w2,~b,bin);
+and (w3,~w1,a);
+or (bout,w1,w2);
 endmodule
